35552ce460e7a155caafa8d8714fe28b0f969590
syukyu8 <s153097@matsu.shimane-u.ac.jp>
2021-06-29 20:23:04 +0900

    learned 6/29 Lesson8

--- main.js
+++ main.js
@@ -1,19 +1,101 @@
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+
+        <div class="product-image">
+            <img v-bind:src="image">
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <!-- <p v-else-if="inventory <=10 && inventory > 0">Almost sold out!</p> -->
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div v-for="(variant, index) in variants" 
+                :key="variant.variantId"
+                class="color-box"
+                :style="{backgroundColor: variant.variantColor}"
+                @mouseover="updateProduct(index)">
+            </div>
+
+            <button v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+                    >Add to Cart</button>
+
+            <div class="cart">
+                <p>Cart({{cart}})</p>
+            </div>
+        </div>
+    
+    </div>
+    `,
+    data(){
+        return {
+            bland: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            details:["80% cotton", "20% polyester", "Gender-neutral"],
+            variants:[   
+                {
+                    variantId:2234,
+                    variantColor: 'green',
+                    variantImage: './assets/vmSocks-green.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId:2235,
+                    variantColor: 'blue',
+                    variantImage: './assets/vmSocks-blue.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart:0
+        }
+    },
+    methods:{
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed:{
+        title(){
+            return this.bland + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping(){
+            if(this.premium){
+                return "Free"
+            }
+            return 2.99
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
-    data:{
-        product: 'Socks',
-        image: "./assets/vmSocks-green.jpg",
-        inStock: true,
-        details:["80% cotton", "20% polyester", "Gender-neutral"],
-        variants:[   
-            {
-                variantId:2234,
-                variantColor: "green"
-            },
-            {
-                variantId:2235,
-                variantColor: "blue"
-            }
-        ]
+    data: {
+        premium: true
     }
 })
